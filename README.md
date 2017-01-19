f09567d886d777826fac0427dde252117594e4d8
gwcmk <gregwcmkim@gmail.com>
2017-01-19 10:17:11 -0600

    Merging personal repo into shared knight lab repo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# studio-shareable-vr
-2017W Knight Lab Studio Class: A Tool for Creating Shareable, Embeddable WebVR Stories
+# aframe-practice
+
+Start
+```
+$ npm install
+$ npm start
+```
---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+const images = require('./controllers/images');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const route = require('koa-route');
+const koa = require('koa');
+const path = require('path');
+const app = module.exports = koa();
+
+// Logger
+app.use(logger());
+
+app.use(route.get('/', images.home));
+app.use(route.get('/aframe/:image', images.aframe));
+app.use(route.get('/helloworld', images.helloworld));
+app.use(route.get('/story/new', images.newStory));
+app.use(route.get('/story/:id', images.show));
+app.use(route.get('/story/edit/:id', images.edit));
+app.use(route.post('/story', images.create));
+app.use(route.post('/story/:id', images.update));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
