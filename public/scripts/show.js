d30a5d1aa8f70b716b4124a7ed683279bff055a8
gwcmk <gregwcmkim@gmail.com>
2017-01-24 12:50:42 -0600

    Made compass with a-frame primitives

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,8 +1,9 @@
+// Fade transitions
 let skyIndex = 1; // swig's loop index starts at 1...
-let storyLength = document.querySelectorAll('a-assets img').length;
-let aSky = document.querySelector('a-sky');
-let aSkyFadeOut = document.querySelector('a-sky #fade-out');
-let aSkyFadeIn = document.querySelector('a-sky #fade-in');
+const storyLength = document.querySelectorAll('a-assets img').length;
+const aSky = document.querySelector('a-sky');
+const aSkyFadeOut = document.querySelector('a-sky #fade-out');
+const aSkyFadeIn = document.querySelector('a-sky #fade-in');
 
 aSkyFadeOut.addEventListener('animationend', () => {
   aSky.setAttribute('src', `#sky-${++skyIndex}`);
@@ -12,7 +13,19 @@ aSkyFadeOut.addEventListener('animationend', () => {
 aSkyFadeIn.addEventListener('animationend', () => {
 });
 
-document.getElementById('next').addEventListener('click', () => {
-  if (skyIndex < storyLength)
-    aSky.emit('fadeOut');
+// document.getElementById('next').addEventListener('click', () => {
+//   if (skyIndex < storyLength)
+//     aSky.emit('fadeOut');
+// });
+
+// Compass
+let angle = 0;
+const pointer = document.getElementById('pointer');
+
+document.getElementById('camera').addEventListener('componentchanged', (event) => {
+  if (event.detail.name === 'rotation' && event.detail.newData.y !== angle) {
+    angle = event.detail.newData.y;
+    pointer.setAttribute('position', `${-1.5 * Math.sin(angle * (Math.PI / 180))} ${1.5 * Math.cos(angle * (Math.PI / 180))} -5`);
+    pointer.setAttribute('rotation', `0 0 ${angle}`);
+  }
 });
