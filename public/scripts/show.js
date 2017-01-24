33e38f1ac1355df8cdcfb2c84e659eca33ce3651
gwcmk <gregwcmkim@gmail.com>
2017-01-24 12:50:42 -0600

    Added compass to show rotation

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,8 +1,9 @@
+// Fade transitions
 let skyIndex = 1; // swig's loop index starts at 1...
-let storyLength = document.querySelectorAll('a-assets img').length;
-let aSky = document.querySelector('a-sky');
-let aSkyFadeOut = document.querySelector('a-sky #fade-out');
-let aSkyFadeIn = document.querySelector('a-sky #fade-in');
+const storyLength = document.querySelectorAll('a-assets img').length;
+const aSky = document.querySelector('a-sky');
+const aSkyFadeOut = document.querySelector('a-sky #fade-out');
+const aSkyFadeIn = document.querySelector('a-sky #fade-in');
 
 aSkyFadeOut.addEventListener('animationend', () => {
   aSky.setAttribute('src', `#sky-${++skyIndex}`);
@@ -15,4 +16,15 @@ aSkyFadeIn.addEventListener('animationend', () => {
 document.getElementById('next').addEventListener('click', () => {
   if (skyIndex < storyLength)
     aSky.emit('fadeOut');
+});
+
+// Compass
+let angle = 0;
+const pointer = document.getElementById('pointer');
+
+document.getElementById('camera').addEventListener('componentchanged', (event) => {
+  if (event.detail.name === 'rotation' && event.detail.newData.y !== angle) {
+    angle = event.detail.newData.y;
+    pointer.style.transform = `rotate(${-angle}deg) translateY(-45px)`;
+  }
 });
