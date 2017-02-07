0623edefc11eb44f66910e6f411a865bd7a8f660
gwcmk <gregwcmkim@gmail.com>
2017-02-07 09:05:01 -0600

    Dynamically position compass based on screen size

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -5,13 +5,13 @@ const aSky = document.querySelector('a-sky');
 const aSkyFadeOut = document.querySelector('a-sky #fade-out');
 const aSkyFadeIn = document.querySelector('a-sky #fade-in');
 
-aSkyFadeOut.addEventListener('animationend', () => {
-  aSky.setAttribute('src', `#sky-${++skyIndex}`);
-  aSky.emit('fadeIn');
-});
+// aSkyFadeOut.addEventListener('animationend', () => {
+//   aSky.setAttribute('src', `#sky-${++skyIndex}`);
+//   aSky.emit('fadeIn');
+// });
 
-aSkyFadeIn.addEventListener('animationend', () => {
-});
+// aSkyFadeIn.addEventListener('animationend', () => {
+// });
 
 // document.getElementById('next').addEventListener('click', () => {
 //   if (skyIndex < storyLength)
@@ -20,12 +20,52 @@ aSkyFadeIn.addEventListener('animationend', () => {
 
 // Compass
 let angle = 0;
-const pointer = document.getElementById('pointer');
+const pointerEl = document.getElementById('pointer');
 
 document.getElementById('camera').addEventListener('componentchanged', (event) => {
   if (event.detail.name === 'rotation' && event.detail.newData.y !== angle) {
     angle = event.detail.newData.y;
-    pointer.setAttribute('position', `${-1.5 * Math.sin(angle * (Math.PI / 180))} ${1.5 * Math.cos(angle * (Math.PI / 180))} -5`);
-    pointer.setAttribute('rotation', `0 0 ${angle}`);
+    pointerEl.setAttribute('position', `${-0.75 * Math.sin(angle * (Math.PI / 180))} ${0.75 * Math.cos(angle * (Math.PI / 180))} 0`);
+    pointerEl.setAttribute('rotation', `0 0 ${angle}`);
   }
 });
+
+
+AFRAME.registerComponent('position-compass', {
+  x: 0,
+  finished: false,
+  frustum: new THREE.Frustum(),
+  init: function () {
+    // on init, construct a frustum
+    // more details here: http://stackoverflow.com/questions/17624021/determine-if-a-mesh-is-visible-on-the-viewport-according-to-current-camera
+    let cameraEl = document.getElementById('camera');
+    let cameraObject3D = cameraEl.getObject3D('camera');
+    let cameraViewProjectionMatrix = new THREE.Matrix4();
+
+    cameraEl.object3D.updateMatrixWorld();
+    cameraObject3D.matrixWorldInverse.getInverse(cameraObject3D.matrixWorld);
+    cameraViewProjectionMatrix.multiplyMatrices(cameraObject3D.projectionMatrix, cameraObject3D.matrixWorldInverse);
+    this.frustum.setFromMatrix(cameraViewProjectionMatrix);
+  },
+  tick: function () {
+    if (!this.finished)
+      this.updatePosition();
+  },
+  updatePosition: function () {
+    let compassEl = document.getElementById('compass');
+    let pointerEl = document.getElementById('pointer');
+    compassEl.setAttribute('position', `${this.x++} 0 -12`);
+
+    // move the compass until it is outside of viewport
+    if (!this.frustum.intersectsObject(pointerEl.getObject3D('mesh'))) {
+      this.finished = true;
+      // TODO: find the source of this error. The position error seems to be consistent for various screen sizes.
+      const error = this.el.isMobile ? 2.5 : -5;
+      compassEl.setAttribute('position', `${this.x + error} 0 -12`);
+      compassEl.emit('fadeIn');
+      pointerEl.emit('fadeIn');
+    }
+  }
+});
+
+
