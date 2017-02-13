dc9ffbdd863240ec8d3f302db33f64cf511396ca
gwcmk <gregwcmkim@gmail.com>
2017-02-12 23:59:02 -0600

    Using HTML overlay instead of a-frame primitives for compass and next button

--- public/scripts/show.js
+++ public/scripts/show.js
@@ -1,6 +1,6 @@
 // Fade transitions
 let skyIndex = 1; // swig's loop index starts at 1...
-const storyLength = document.querySelectorAll('a-assets img').length;
+const storyLength = document.querySelectorAll('a-assets .sky').length;
 const aSky = document.querySelector('a-sky');
 const aSkyFadeOut = document.querySelector('a-sky #fade-out');
 const aSkyFadeIn = document.querySelector('a-sky #fade-in');
@@ -13,19 +13,19 @@ aSkyFadeOut.addEventListener('animationend', () => {
 aSkyFadeIn.addEventListener('animationend', () => {
 });
 
-// document.getElementById('next').addEventListener('click', () => {
-//   if (skyIndex < storyLength)
-//     aSky.emit('fadeOut');
-// });
+document.getElementById('next').addEventListener('click', () => {
+  if (skyIndex < storyLength)
+    aSky.emit('fadeOut');
+});
 
 // Compass
-let angle = 0;
-const pointer = document.getElementById('pointer');
+let angle, angleInRadians = 0;
+const pointerEl = document.getElementById('pointer');
 
 document.getElementById('camera').addEventListener('componentchanged', (event) => {
   if (event.detail.name === 'rotation' && event.detail.newData.y !== angle) {
     angle = event.detail.newData.y;
-    pointer.setAttribute('position', `${-1.5 * Math.sin(angle * (Math.PI / 180))} ${1.5 * Math.cos(angle * (Math.PI / 180))} -5`);
-    pointer.setAttribute('rotation', `0 0 ${angle}`);
+    angleInRadians = angle * (Math.PI / 180);
+    pointerEl.style.transform = `translate(${-30 * Math.sin(angleInRadians)}px, ${-30 * Math.cos(angleInRadians)}px) rotateZ(${-angle}deg)`
   }
 });
