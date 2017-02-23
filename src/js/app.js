ab4782f07f865b264aaeceb62541f45f7de73d4f
gwcmk <gregwcmkim@gmail.com>
2017-02-23 11:28:55 -0600

    Using mock authoring spreadsheet

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,16 @@ const ui = require('ui.js');
 const gapiClient = require('gapiClient.js');
 const template = require('template.js');
 
-const spreadsheetId = '1aLMe-s3SGIPZw51prsyNvM8LENNtI55WP0b7LAyLajE';
+const spreadsheetId = '1tTyJECAoiLjHQh9_RWtVavhO_rLk133dt8-5c6lN-F0';
 
 function initalize() {
   gapiClient.getSpreadsheetData(spreadsheetId).then(response => {
     let templateData = { images: [] };
     response.entry.forEach(e => {
-      templateData.images.push({ path: e.gsx$paths.$t });
+      templateData.images.push({
+        path: e.gsx$sceneimageurl.$t,
+        text: e.gsx$bodytext.$t
+      });
     });
 
     let scene = template.buildTemplate(templateData);
