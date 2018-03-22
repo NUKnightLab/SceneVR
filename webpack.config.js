49e705cbb5283e77cc8687717196afe632fc8a5c
Zach Wise <zach@digitalartwork.net>
2018-03-22 13:26:22 -0500

    Forgot to commit this.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
 var webpack = require('webpack'),
-    path = require('path'),
+	path = require('path'),
 
-    componentPath = path.resolve('./src/js');
+	componentPath = path.resolve('./src/js');
 
 module.exports = {
-  context: path.join(__dirname),
-  entry: [
-    "./src/js/scene.js"
-  ],
-  output: {
-    path: path.join(__dirname, "./dist/js"),
-    filename: "[name].js"
-  },
-  resolve: {
-    root: componentPath
-  },
-  resolveLoader: {
-    root: path.join(__dirname, "node_modules")
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  }
+	context: path.join(__dirname),
+	entry: {
+		scene: "./src/js/scene.js"
+	},
+	output: {
+		path: path.join(__dirname, "./dist/js"),
+		filename: "[name].js"
+	},
+	resolve: {
+		root: componentPath
+	},
+	resolveLoader: {
+		root: path.join(__dirname, "node_modules")
+	},
+	module: {
+		loaders: [{
+			loader: 'babel-loader',
+			query: {
+				presets: ['es2015']
+			}
+		}]
+	}
 }
