13f157783a7bca843cad9ed580c1c3daca4f681b
Zach Wise <zach@digitalartwork.net>
2018-06-19 23:50:02 -0500

    Setting correct size for iPhone panoramas

--- src/js/ui/Pano.js
+++ src/js/ui/Pano.js
@@ -6,6 +6,7 @@ module.exports = class Pano {
     constructor(data) {
         this.data = data;
         this.geometry = new THREE.SphereBufferGeometry( 1000, 60, 40 );
+
         this.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
         this.mesh = {};
         this.texture_loader = new THREE.TextureLoader();
@@ -14,6 +15,7 @@ module.exports = class Pano {
         this.active = false;
         this.animation_time = 1;
         this.events = new EventEmitter();
+        this.geometry_fixed = false;
 
 
         this.geometry.scale( - 1, 1, 1 );
@@ -65,11 +67,40 @@ module.exports = class Pano {
         }});
     }
 
+    fixGeometry(w, h) {
+        if (!this.geometry_fixed) {
+            let image_multiplier = (w / h) * 60.8;
+            image_multiplier = THREE.Math.degToRad(image_multiplier)
+            // let image_multiplier = ( ( (texture.image.width / texture.image.height) * 60.8) * Math.PI)/180;
+            let thetaLength = (60.8 * Math.PI)/180
+            console.log(`image_multiplier ${image_multiplier}`)
+            console.log(THREE.Math.degToRad(image_multiplier) )
+            let g = {
+                radius: 1000,
+                widthSegments: 60,
+                heightSegments: 40,
+                phiStart: Math.PI*2,
+                phiLength: Math.PI*(image_multiplier/2),
+                thetaStart: 45,
+                thetaLength: Math.PI/2
+            }
+            this.geometry = new THREE.SphereBufferGeometry( g.radius, g.widthSegments, g.heightSegments, g.phiStart, g.phiLength, g.thetaStart, g.thetaLength);
+            this.geometry.scale( - 1, 1, 1 );
+            this.mesh.geometry = this.geometry;
+            this.mesh.geometry.rotateY((180 * Math.PI)/180);
+            this.geometry_fixed = true;
+        }
+
+    }
+
     loadTexture(url) {
         return new Promise((resolve, reject) => {
-            this.texture_loader.load(url, function(texture) {
-                console.log(`${texture.image.width} Width and ${texture.image.height} Height`);
-                console.log(texture.image.height/texture.image.width)
+            this.texture_loader.load(url, (texture) => {
+                // check if equilinear
+                if ((texture.image.height/texture.image.width) < 0.45) {
+                    console.log("is not equilinear")
+                    this.fixGeometry(texture.image.width, texture.image.height);
+                }
                 resolve(new THREE.MeshBasicMaterial( {
                     map: texture
                 }));
