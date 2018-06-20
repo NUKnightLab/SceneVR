b52426734e331c16f3b78ab025356c52279c173b
Zach Wise <zach@digitalartwork.net>
2018-06-20 11:17:28 -0500

    logic for ui hiding and showing

--- src/js/ui/Scene.js
+++ src/js/ui/Scene.js
@@ -20,13 +20,9 @@ module.exports = class Scene {
         this.pointer = {
             down_x: 0,
             down_y: 0,
-            lon: 0,
-            lat: 0
+            move_x: 0,
+            move_y: 0
         };
-        this.pos = {
-            lat: 0,
-            lon: 0
-        }
         this.loaded = [];
 
 
@@ -63,14 +59,18 @@ module.exports = class Scene {
     }
 
     startListening() {
-        // this.el.container.addEventListener('mousedown', (e) => {this.onMouseDown(e)});
+        this.stage.el.addEventListener('mousedown', (e) => {this.onMouseDown(e)});
+        this.stage.el.addEventListener('mouseup', (e) => {this.onMouseUp(e)});
         // this.el.container.addEventListener('mousemove', (e) => {this.onMouseMove(e)});
-        // this.el.container.addEventListener('mouseup', (e) => {this.onMouseUp(e)});
         // this.el.container.addEventListener('touchstart', (e) => {this.onTouchStart(e)});
         // this.el.container.addEventListener('touchmove', (e) => {this.onTouchMove(e)});
-        this.el.container.addEventListener('dblclick', (e) => {this.onMouseDoubleClick(e)});
+
         this.chrome.events.addListener("fullscreen", (e) => {
             this.fullScreenToggle(e);
+
+        })
+        this.chrome.events.addListener("goto", (e) => {
+            this.goTo(e.number);
 
         })
 
@@ -78,10 +78,6 @@ module.exports = class Scene {
 
     fullScreenToggle(e) {
         console.log("FULLSCREEN")
-        // if (this.el.container.requestFullscreen) {
-        //     this.el.container.requestFullscreen();
-        // }
-        // this.el.container.requestFullscreen();
         if (this.el.container.requestFullscreen) {
             this.el.container.requestFullscreen();
         } else if (this.el.container.webkitRequestFullscreen) {
@@ -93,46 +89,21 @@ module.exports = class Scene {
         }
         this.updateSize();
     }
-    onMouseDoubleClick(e) {
-        console.log("DOUBLE CLICK")
-        let new_pano = this.current_pano + 1;
-        this.goTo(new_pano);
+
+
+    onMouseDown(e) {
+        this.pointer.down_x = e.clientX;
+        this.pointer.down_y = e.clientY;
     }
-    //
-    // onMouseDown(e) {
-    //     e.preventDefault();
-    //     this.user_interacting = true;
-    //     console.log(e);
-    //     console.log(this.pointer)
-    //     this.pointer.down_x = e.clientX;
-    //     this.pointer.down_y = e.clientY;
-    //
-    //     this.pointer.lon = this.stage.pos.lon;
-    //     this.pointer.lat = this.stage.pos.lat;
-    //
-    //     console.log("onMouseDown")
-    // }
-    //
-    // onMouseUp(e) {
-    //     this.user_interacting = false;
-    //     console.log("onMouseUp")
-    // }
-    //
-    // onMouseMove(e) {
-    //     if (this.user_interacting) {
-    //
-    //         this.stage.pos.lon = ( this.pointer.down_x - e.clientX ) * 0.1 + this.pointer.lon;
-	// 		this.stage.pos.lat = ( e.clientY - this.pointer.down_y ) * 0.1 + this.pointer.lat;
-    //     }
-    // }
-    //
-    // onTouchStart(e) {
-    //
-    // }
-    //
-    // onTouchMove(e) {
-    //
-    // }
+
+    onMouseUp(e) {
+        this.pointer.move_x = Math.abs(this.pointer.down_x - e.clientX);
+        this.pointer.move_y = Math.abs(this.pointer.down_y - e.clientY);
+
+        if (this.pointer.move_x < 10 && this.pointer.move_y < 10 ) {
+            this.chrome.toggleUI();
+        }
+    }
 
     buildPanos() {
         for (let i = 0; i < this.data.scenes.length; i++) {
