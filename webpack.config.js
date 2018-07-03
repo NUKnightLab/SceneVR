c8c329e684e264a7759ef17fe02dd2a804ad2db4
Zach Wise <zach@digitalartwork.net>
2018-07-03 14:56:45 -0500

    Tweak uglify options. JS is now down to 780k!

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
-var webpack = require('webpack'),
-    path = require('path');
-
+const webpack = require('webpack'),
+      path = require('path'),
+      UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  context: path.join(__dirname),
-  entry: {
-    scene: "./src/js/scenevr.js"
-  },
-  output: {
-    path: path.join(__dirname, "./dist/js"),
-    filename: "[name].js"
-  },
-  resolve: {
-    modules: [ path.resolve('./src/js'), "node_modules" ]
-  },
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['babel-preset-env']
-          }
-        }
-      }
-    ]
-  }
+    context: path.join(__dirname),
+    entry: {
+        scene: "./src/js/scenevr.js"
+    },
+    output: {
+        path: path.join(__dirname, "./dist/js"),
+        filename: "[name].js"
+    },
+    resolve: {
+        modules: [path.resolve('./src/js'), "node_modules"]
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['babel-preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimize:true,
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    ecma: 6,
+                    warnings: false,
+                    compress: {
+                        unused: true,
+                        dead_code: true
+                    },
+                    ie8: false,
+                    keep_classnames: false,
+                    output: {
+                        beautify: false,
+                        comments: false
+                    }
+                }
+            })
+        ]
+    }
 }
