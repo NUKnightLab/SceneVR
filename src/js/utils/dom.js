19a59bc87f5f20865aa30b49bf38c55ede1c5458
Zach Wise <zach@digitalartwork.net>
2018-07-05 14:53:51 -0500

    add ability to create element with content easily

--- src/js/utils/dom.js
+++ src/js/utils/dom.js
@@ -1,5 +1,5 @@
 module.exports = {
-    createElement: (tag, id, classes, add_to_container) => {
+    createElement: (tag, id, classes, add_to_container, content) => {
         let el = document.createElement(tag);
         if(id) {
             el.id = id;
@@ -10,6 +10,9 @@ module.exports = {
         if (add_to_container) {
             add_to_container.appendChild(el);
         };
+        if (content) {
+            el.innerHTML = content;
+        }
         return el;
     }
 }
