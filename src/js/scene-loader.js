fe15279e643813ebd685c0c3ef99e0421da5967e
Joe Germuska <joe@germuska.com>
2018-07-14 20:00:01 -0500

    interim shifting to class based stuff

--- src/js/scene-loader.js
+++ src/js/scene-loader.js
@@ -1,7 +1,5 @@
 const ProgressCube = require('./ui/ProgressCube.js');
 
-let scene = {};
-
 let svr_config = {
     source: "/assets/test_panos/data.json",
     speed: "m"
@@ -15,6 +13,8 @@ let svr_time = {
 }
 
 function svr_init() {
+    console.log("oldskool")
+
     // LOAD URL PARAMETERS
     function getQueryParams(query_params) {
         query_params = query_params.split("+").join(" ");
@@ -70,9 +70,64 @@ function svr_init_scene() {
     console.debug(`${message} SceneVR will use ${svr_config.speed} images`);
     document.getElementById("svr-loading-message").innerHTML = message;
 
-    window.init_scene(svr_config);
-
-    // scene = new window.SceneVR(svr_config);
+    Scene.init_scene(window, svr_config);
 }
 
-window.onload = svr_init;
+// window.onload = svr_init;
+
+window.SceneLoader = class SceneLoader {
+    constructor(scenevr_url, config) {
+        this.scenevr_url = scenevr_url;
+        this.config = config;
+
+        this.svr_time = {
+            js_size: 1598345, //bytes
+            estimated: 0,
+            start: 0,
+            end: 0
+        }
+
+
+    }
+
+    load_scenevr() {
+        console.log("class based")
+        let script = document.createElement('script');
+        var self = this;
+        script.onload = function() {
+            self.make_scene();
+        }
+        this.svr_time.start = (new Date()).getTime();
+
+        script.src = this.scenevr_url;
+
+        document.head.appendChild(script);
+
+        let progress = new ProgressCube();
+    }
+
+    make_scene() {
+        this.svr_time.end = (new Date()).getTime();
+
+        let duration = (this.svr_time.end - this.svr_time.start) / 1000,
+            bitsLoaded = this.svr_time.js_size * 8,
+            speedBps = (bitsLoaded / duration).toFixed(2),
+            speedKbps = (speedBps / 1024).toFixed(2),
+            speedMbps = (speedKbps / 1024).toFixed(2),
+            message = `connection speed ${speedMbps}Mbps`;
+
+        if (speedMbps > 10) {
+            this.config.speed = "l";
+        } else if (speedMbps > 5) {
+            this.config.speed = "m";
+        } else {
+            this.config.speed = "s";
+        }
+
+        console.debug(`${message} SceneVR will use ${svr_config.speed} images`);
+        document.getElementById("svr-loading-message").innerHTML = message;
+
+        Scene.init_scene(window, this.config);        
+    }
+
+}
